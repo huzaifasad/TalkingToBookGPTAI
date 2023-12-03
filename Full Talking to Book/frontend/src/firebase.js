a039d62accf72f582bffabd2aa4e4926a0af85e8
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2023-12-03 02:36:35 -0800

    Update firebase.js

--- Full Talking to Book/frontend/src/firebase.js
+++ Full Talking to Book/frontend/src/firebase.js
@@ -17,13 +17,7 @@ import {
   browserSessionPersistence,
 } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyD1cSPOw2njz63EZIwS_4FvngsAHOcV-KU",
-  authDomain: "fivvernew-a11d9.firebaseapp.com",
-  projectId: "fivvernew-a11d9",
-  storageBucket: "fivvernew-a11d9.appspot.com",
-  messagingSenderId: "306901686357",
-  appId: "1:306901686357:web:e75ba58c1bb0e1c13d5cdc",
-  measurementId: "G-VR1T5K2F6L",
+  //Your Firebase API Key
 };
 
 const app = initializeApp(firebaseConfig);
